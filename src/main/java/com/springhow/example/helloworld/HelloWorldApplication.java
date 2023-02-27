cf71c2d17c161816323669202f07067542f9067f
saitejatamiri <98472448+saitejatamiri@users.noreply.github.com>
2023-02-26 23:28:45 -0800

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. hello world";
+        return "5. hello worlds";
     }
 
 }
